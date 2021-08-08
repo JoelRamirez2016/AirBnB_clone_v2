33e15c8b42b1d276f903670f5e34ce12ffab80e3
Orcha02 <orcha2002@gmail.com>
2021-08-08 20:02:59 +0000

    Se revirtieron los cambios

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Amenity Class"""
-     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+class Amenity(BaseModel):
+    name = ""
