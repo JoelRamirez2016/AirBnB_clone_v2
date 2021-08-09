b8ee4b347be24a2454cc9bcfcc232e0e3c57277d
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-09 16:20:04 +0000

    fix console for check 6

--- console.py
+++ console.py
@@ -221,10 +221,10 @@ class HBNBCommand(cmd.Cmd):
                 return
             for k, v in storage.all().items():
                 if k.split('.')[0] == args:
-                    print_list.append(v)
+                    print_list.append(str(v))
         else:
             for k, v in storage.all().items():
-                print_list.append(v)
+                print_list.append(str(v))
 
         print(print_list)
 
