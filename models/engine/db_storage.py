95076451a1e19df87da7e14a25a9e37c05dd835a
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-11 20:10:20 +0000

    uncomend Amenity from all db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -43,7 +43,7 @@ class DBStorage:
             objs += self.__session.query(City).all()
             objs += self.__session.query(User).all()
             objs += self.__session.query(Place).all()
-#            objs += self.__session.query(Amenity).all()
+            objs += self.__session.query(Amenity).all()
             objs += self.__session.query(Review).all()
         else:
             objs += self.query(cls).all()
