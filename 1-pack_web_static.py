2f9eca83cbc47033738afb77f017bb65d1b98a68
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-27 04:42:57 +0000

    fix 1-pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ Define Funtion do_pack
 """
 from datetime import datetime
 from fabric.api import local
+import os
 
 
 def do_pack():
@@ -14,6 +15,10 @@ def do_pack():
     dt = datetime.now()
     f = "versions/web_static_{}.tgz".format(dt.strftime("%Y%m%d%H%M%S"))
 
+    if not os.path.isdir("versions") and local("mkdir -p versions").failed:
+        return None
+
     if local("tar -cvzf {} web_static".format(f)).failed:
         return None
+
     return f
