8d600fa77b7e460257d46b3954b624793039068c
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-12 03:45:52 +0000

    update models and engine

--- models/state.py
+++ models/state.py
@@ -8,3 +8,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ Getter of cities"""
+            cities = models.storage.all(City)
+            list_cities = []
+
+            for c in cities.values():
+                if self.id == c.place_id:
+                    list_cities.append(c)
+
+            return list_cities
