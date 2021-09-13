a1959217a9357bcefc6be8384a5c7595958679cb
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-13 15:23:28 +0000

    add 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """display a HTML page with a list of states"""
+    return render_template("7-states_list.html", states=storage.all("State"))
+
+@app.teardown_appcontext
+def teardown(e):
+    """close the current session."""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run()
