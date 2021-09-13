5eebf819cede30090cdec37eb27d90f567f189b2
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-13 15:28:05 +0000

    fix 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """display a HTML page with a list of states"""
-    return render_template("7-states_list.html", states=storage.all("State"))
+    return render_template("7-states_list.html", states=storage.all(State))
 
 @app.teardown_appcontext
 def teardown(e):
